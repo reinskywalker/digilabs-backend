e7f5ed6e7c57eaf4feb310832b53a38d254eb20f
Reynaldi Lusikooy <andre.reynaldi@ai.astra.co.id>
2024-10-10 21:13:52 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AutoCompleteController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+Route::middleware(["auth", 'role:superadmin'])->group(function () {
+    Route::apiResource('users', UserController::class)->middleware('role: superadmin');
+
+    Route::get('autocomplete/{type}', [AutoCompleteController::class, 'autocomplete']);
+    Route::post('autocomplete/{type}', [AutoCompleteController::class, 'autocomplete']);
+});
+
+require __DIR__ . '/auth.php';
