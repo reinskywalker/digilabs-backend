a59744bfa58e32028a5664584f36bb638d365955
reinskywalker <reynaldi@outlook.jp>
2024-10-17 18:42:43 +0700

    exclude cors base origin

--- config/cors.php
+++ config/cors.php
@@ -17,11 +17,16 @@ return [
 
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        'http://103.47.132.55',
+        'http://3.1.220.199',
+        'https://vercel.live/link/digilabs-frontend-kyrie-lucids-projects.vercel.app?via=project-dashboard-alias-list&p=1',
+        'https://*.vercel.app',
+        env('FRONTEND_URL', 'http://localhost:3000'),
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
-
 ];
