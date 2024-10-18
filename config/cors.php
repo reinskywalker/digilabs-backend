55a43d573003e2601f14e8975d705cca761b5ab5
reinskywalker <reynaldi@outlook.jp>
2024-10-18 08:23:23 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -1,32 +1,16 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://103.47.132.55',
-        'http://3.1.220.199',
-        'https://vercel.live/link/digilabs-frontend-kyrie-lucids-projects.vercel.app?via=project-dashboard-alias-list&p=1',
-        'https://*.vercel.app',
-        env('FRONTEND_URL', 'http://localhost:3000'),
+        'http://localhost:3000',  // Localhost for development
+        'http://103.47.132.55',    // Your IP address
+        'http://3.1.220.199',      // Your server's IP address
+        'https://*.vercel.app',    // Vercel frontend, if applicable
     ],
-    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => true, // This should be true to allow cookies
 ];
